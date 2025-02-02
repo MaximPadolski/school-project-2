85405c73dd6077b270ca134ddb90fc76e1a6e583
Maksim Padolski <maxim.padolski@gmail.com>
2025-02-02 20:19:50 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,59 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CdkDragDrop, DragDropModule, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { DATA } from './data';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [DragDropModule, CommonModule
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {
-  title = 'school-2';
+  public readonly questions = DATA;
+  public readonly box1 = [{q: 'море', a: 'сущ'}];
+  public readonly box2 = [{q: 'море', a: 'сущ'}];
+  public readonly className = signal({key: '', value: ''});
+
+
+  // todo = ['Get to work', 'Pick up groceries', 'Go home', 'Fall asleep'];
+
+  // done = ['Get up', 'Brush teeth', 'Take a shower', 'Check e-mail', 'Walk dog'];
+
+  drop(event: any, ) {
+    const item = event.previousContainer.data[event.previousIndex];
+
+    // console.log('drop event', { event, item });
+
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+
+      if ((event.container.id === 'cdk-drop-list-1' && item.a === 'сущ') ||
+          (event.container.id === 'cdk-drop-list-2' && item.a === 'прил')
+      ) {
+        transferArrayItem(
+          event.previousContainer.data,
+          event.container.data,
+          event.previousIndex,
+          event.currentIndex,
+        );
+        this.setClassName(event.container.id, 'correct');
+      } else {
+        this.setClassName(event.container.id, 'wrong');
+      }
+
+    }
+  }
+
+  private setClassName(key: string, value: string): void {
+    this.className.set({key, value});
+    setTimeout(() => {
+      this.className.set({key, value: ''});
+    }, 1500);
+  }
 }
